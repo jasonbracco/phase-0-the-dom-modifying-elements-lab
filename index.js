70ed9a2f0a4efae7f714dacb937fedc279b92140
Jason Bracco <bracco.jason@gmail.com>
2022-05-11 18:01:43 -0400

    commit tree 78c6cb9a367659c1d79e4b85c941c5e050eb32a7

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+document.body.append(newHeader.textContent = "Jason is the champion" )
+//newHeader.textContent = "Jason is the champion"
+
+
